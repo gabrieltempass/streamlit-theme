d6a33a12dee167ea0096ea61678348e4d2d8b773
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2024-03-09 19:18:14 -0300

    feat: Simplify and separate examples

---
+++ examples/st_theme_1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from streamlit_theme import st_theme
+
+theme = st_theme()
+st.write(theme)
---
+++ examples/st_theme_2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit_theme import st_theme
+
+adjust = st.toggle("Make the CSS adjustment")
+
+st.write("Input:")
+st.code(
+	f"""
+	st.write("Lorem ipsum")
+	st_theme(adjust={adjust})
+	st.write("Lorem ipsum")
+	"""
+)
+
+st.write("Output:")
+st.write("Lorem ipsum")
+st_theme(adjust=adjust)
+st.write("Lorem ipsum")
