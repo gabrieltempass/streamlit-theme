6c82510365a9e03502b0042d725ddf05c6ccb654
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2024-03-09 22:41:34 -0300

    fix: Convert indentation to spaces

--- examples/st_theme_2.py
+++ examples/st_theme_2.py
@@ -5,11 +5,11 @@ adjust = st.toggle("Make the CSS adjustment")
 
 st.write("Input:")
 st.code(
-	f"""
-	st.write("Lorem ipsum")
-	st_theme(adjust={adjust})
-	st.write("Lorem ipsum")
-	"""
+    f"""
+    st.write("Lorem ipsum")
+    st_theme(adjust={adjust})
+    st.write("Lorem ipsum")
+    """
 )
 
 st.write("Output:")
--- streamlit_theme/example.py
+++ streamlit_theme/example.py
@@ -9,14 +9,14 @@ adjust = st.toggle("Make the CSS adjustment", value=True)
 col1, col2 = st.columns(2)
 
 with col1:
-	st.write("Output:")
-	st.write("Lorem ipsum")
-	theme = st_theme(adjust=adjust)
-	st.write("Lorem ipsum")
+    st.write("Output:")
+    st.write("Lorem ipsum")
+    theme = st_theme(adjust=adjust)
+    st.write("Lorem ipsum")
 
 with col2:
-	st.write("Reference:")
-	st.write("Lorem ipsum")
-	st.write("Lorem ipsum")
+    st.write("Reference:")
+    st.write("Lorem ipsum")
+    st.write("Lorem ipsum")
 
 st.write(theme)
