7e896b31142378748466df2abd3f17ec3d20dcf2
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2024-03-10 22:29:47 -0300

    fix: Put correct dependecy

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
     ],
     python_requires=">=3.8",
-    install_requires=["streamlit >= 0.63"],
+    install_requires=["streamlit >= 1.29"],
 )
