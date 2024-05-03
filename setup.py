a93aa526f17123171ae1f071bcc840b2cf938c25
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2024-05-03 16:21:39 -0300

    chore: Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="st-theme",
-    version="1.2.2",
+    version="1.2.3",
     description="A component that returns the active theme of the Streamlit app.",
     long_description=long_description,
     long_description_content_type="text/markdown",
