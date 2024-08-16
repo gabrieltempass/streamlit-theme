3f94c7844b7fc944bc7ceaea9b3731d90ec2c5ee
Gabriel Tem Pass <41839984+gabrieltempass@users.noreply.github.com>
2024-08-15 23:33:54 -0300

    test: Monitor doc demos in New Relic

---
+++ health_monitor/scriptedBrowser1.js
@@ -0,0 +1,27 @@
+const URL = "https://st-theme-1.streamlit.app"
+const TITLE = "streamlitApp"  // HTML title attribute of the desired iframe.
+const TEXT = "primaryColor"
+
+// Set timeouts for page load and element finding.
+await $webDriver.manage().setTimeouts({
+  pageLoad: 30000,  // 30 seconds for page load timeout.
+  implicit: 5000,  // 5 seconds for element finding timeout.
+});
+
+console.log("Starting the synthetic script.")
+
+console.log("Navigating to:", URL)
+await $webDriver.get(URL)
+
+console.log("Finding the iframe with the title:", TITLE)
+var iframe = await $webDriver.findElement(
+  $selenium.By.xpath("//iframe[@title='" + TITLE + "']")
+)
+
+console.log("Switching to the iframe.")
+await $webDriver.switchTo().frame(iframe)
+
+console.log("Checking for the presence of an element with the text:", TEXT)
+await $webDriver.findElement($selenium.By.xpath("//*[text()='" + TEXT + "']"))
+
+console.log("Script completed successfully.")
